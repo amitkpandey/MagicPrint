20feca4bf170a359adef09ff24be358879c5a3cf
Kshitij Singh <singh_kshitij@yahoo.com>
2018-02-05 11:08:49 -0800

    Final Commit

---
+++ app/src/main/java/com/beingdev/magicprint/HelpCenter.java
@@ -0,0 +1,27 @@
+package com.beingdev.magicprint;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class HelpCenter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_center);
+
+        Toolbar toolbar = findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
